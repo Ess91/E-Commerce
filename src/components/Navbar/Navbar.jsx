70fe951a11d68e3456697038c275831b9f4f6afd
Esther <eagyare91@gmail.com>
2021-02-13 14:02:54 +0000

    cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({ totalItems }) => {
                     <div className={classes.grow} />
                     <div className={classes.button}>
                         <IconButton aria-label="Show cart item" color="inherit">
-                            <Badge badgeContent={2} color="secondary">
+                            <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
